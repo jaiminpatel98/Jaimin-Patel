3589560b2a79ab6889b8ea36528fd12447d54df9
jaiminpatel98 <jaiminpatel98@gmail.com>
2017-06-23 15:43:44 -0400

    added fixed menu

--- portfolio.js
+++ portfolio.js
@@ -22,3 +22,40 @@ $(document).ready(function(){
             } // End if
           });
         });
+
+(function($) {          
+    $(document).ready(function(){                    
+        $(window).scroll(function(){                          
+            if ($(this).scrollTop() > 50) {
+                $('#menu').fadeIn(500);
+            } else {
+                $('#menu').fadeOut(500);
+            }
+        });
+    });
+})(jQuery);
+
+function myFunction(x) {
+    x.classList.toggle("change");
+}
+
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction2() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+//window.onclick = function(event) {
+//  if (!event.target.matches('.dropbtn-nav')) {
+//
+//    var dropdowns = document.getElementsByClassName("dropdown-content-nav");
+//    var i;
+//    for (i = 0; i < dropdowns.length; i++) {
+//      var openDropdown = dropdowns[i];
+//      if (openDropdown.classList.contains('show')) {
+//        openDropdown.classList.remove('show');
+//      }
+//    }
+//  }
+//}
