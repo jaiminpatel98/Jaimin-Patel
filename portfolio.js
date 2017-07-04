48ba88762e5e68517805530b20feb40d664b6772
jaiminpatel98 <jaiminpatel98@gmail.com>
2017-07-04 12:41:09 -0400

    added a portrait album gallery

--- portfolio.js
+++ portfolio.js
@@ -1,46 +1,33 @@
 $(document).ready(function(){
-          // Add smooth scrolling to all links
-          $("a").on('click', function(event) {
+  $("a").on('click', function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+        window.location.hash = hash;
+      });
+    }
+  });
+});
 
-            // Make sure this.hash has a value before overriding default behavior
-            if (this.hash !== "") {
-              // Prevent default anchor click behavior
-              event.preventDefault();
-
-              // Store hash
-              var hash = this.hash;
-
-              // Using jQuery's animate() method to add smooth page scroll
-              // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-              $('html, body').animate({
-                scrollTop: $(hash).offset().top
-              }, 800, function(){
-
-                // Add hash (#) to URL when done scrolling (default click behavior)
-                window.location.hash = hash;
-              });
-            } // End if
-          });
-        });
-
-(function($) {          
-    $(document).ready(function(){                    
-        $(window).scroll(function(){                          
-            if ($(this).scrollTop() > 100) {
-                $('#menu').fadeIn(500);
-            } else {
-                $('#menu').fadeOut(500);
-            }
-        });
+(function($) {
+  $(document).ready(function(){
+    $(window).scroll(function(){
+      if ($(this).scrollTop() > 100) {
+        $('#menu').fadeIn(500);
+      } else {
+        $('#menu').fadeOut(500);
+      }
     });
+  });
 })(jQuery);
 
 function myFunction(x) {
-    x.classList.toggle("change");
+  x.classList.toggle("change");
 }
 
-/* When the user clicks on the button, 
-toggle between hiding and showing the dropdown content */
 function myFunction2() {
-    document.getElementById("myDropdown").classList.toggle("show");
+  document.getElementById("myDropdown").classList.toggle("show");
 }
