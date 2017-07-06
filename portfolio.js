7997f92bf76a35ba101bc50aa88a30db9e6c92e8
jaiminpatel98 <jaiminpatel98@gmail.com>
2017-07-06 10:33:58 -0400

    rewrite of JS file

--- portfolio.js
+++ portfolio.js
@@ -1,28 +1,28 @@
-$(document).ready(function(){
-  $("a").on('click', function(event) {
-    if (this.hash !== "") {
+var main = function() {
+  $('a').on('click', function(event) {
+    if (this.hash !== '') {
       event.preventDefault();
       var hash = this.hash;
       $('html, body').animate({
         scrollTop: $(hash).offset().top
-      }, 800, function(){
+      }, 800, function() {
         window.location.hash = hash;
       });
     }
   });
-});
+}
+$(document).ready(main);
 
-(function($) {
-  $(document).ready(function(){
-    $(window).scroll(function(){
-      if ($(this).scrollTop() > 100) {
-        $('#menu').fadeIn(500);
-      } else {
-        $('#menu').fadeOut(500);
-      }
-    });
-  });
-})(jQuery);
+var main2 = function() {
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 100) {
+      $('#menu').fadeIn(500);
+    } else {
+      $('#menu').fadeOut(500);
+    }
+  })
+}
+$(document).ready(main2);
 
 function myFunction(x) {
   x.classList.toggle("change");
