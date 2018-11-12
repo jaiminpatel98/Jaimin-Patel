b243e11361c3a84f2945b468b5c0a12a9d577cc5
jaiminpatel98 <jaiminpatel98@gmail.com>
2018-11-12 12:57:34 -0500

    cleaned up code formatting - looks pretty :)

--- gallery.js
+++ gallery.js
@@ -1,34 +1,44 @@
 var smoothScroll = function() {
-    $('a').on('click', function(event) {
-      if (this.hash !== '') {
-        event.preventDefault();
-        var hash = this.hash;
-        $('html, body').animate({
-          scrollTop: $(hash).offset().top
-        }, 800, function() {
-          window.location.hash = hash;
-        });
-      }
-    });
-  }
-  $(document).ready(smoothScroll);
-  
-  var menuFade = function() {
-    $(window).scroll(function() {
-      if ($(this).scrollTop() > 100) {
-        $('#menu').fadeIn(500);
-      } else {
-        $('#menu').fadeOut(500);
-      }
-    })
-  }
-  $(document).ready(menuFade);
-  
-  function myFunction(x) {
-    x.classList.toggle("change");
-  }
-  
-  function myFunction2() {
-    document.getElementById("myDropdown").classList.toggle("show");
-  }
-  
+	$('a')
+		.on('click', function(event) {
+			if (this.hash !== '') {
+				event.preventDefault();
+				var hash = this.hash;
+				$('html, body')
+					.animate({
+						scrollTop: $(hash)
+							.offset()
+							.top
+					}, 800, function() {
+						window.location.hash = hash;
+					});
+			}
+		});
+}
+$(document)
+	.ready(smoothScroll);
+
+var menuFade = function() {
+	$(window)
+		.scroll(function() {
+			if ($(this)
+				.scrollTop() > 100) {
+				$('#menu')
+					.fadeIn(500);
+			} else {
+				$('#menu')
+					.fadeOut(500);
+			}
+		})
+}
+$(document)
+	.ready(menuFade);
+
+function myFunction(x) {
+	x.classList.toggle("change");
+}
+
+function myFunction2() {
+	document.getElementById("myDropdown")
+		.classList.toggle("show");
+}
