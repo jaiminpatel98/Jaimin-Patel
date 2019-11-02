c58651efb991d8e5b0b18d23cb8aeca2d2c48f0e
Jaimin Patel <jaiminpatel98@gmail.com>
2019-11-02 16:23:48 -0400

    changed site layout to one page

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
+var main = function() {
+	var acc = document.getElementsByClassName("accordion");
+	var i;
+	for (i = 0; i < acc.length; i++) {
+		acc[i].onclick = function() {
+			this.classList.toggle("active");
+			var panel = this.nextElementSibling;
+			if (panel.style.maxHeight) {
+				panel.style.maxHeight = null;
+			} else {
+				panel.style.maxHeight = panel.scrollHeight + "px";
+			}
+		}
+	}
+}
+$(document).ready(main);
+
 var smoothScroll = function() {
 	$('a')
 		.on('click', function(event) {
